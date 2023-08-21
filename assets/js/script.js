93ab3f09d7ba162d407403c6c301f979d8783bb7
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-08-21 19:55:04 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,8 @@
 window.addEventListener('load', ()=>{
     const preloader = document.querySelector('.preloader');
 
-    preloader.classList.add('preloader-hidden');
+    // preloader.classList.add('preloader-hidden');
     preloader.addEventListener('transitionend', ()=>{
         document.body.removeChild('preloader')
     })
-    console.log(preloader);
 })
